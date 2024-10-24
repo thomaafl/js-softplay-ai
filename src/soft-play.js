a43b80bcb43d5fae9faf56cf4a8ecb7ba247f1c8
Thomas Aleksander Flier <thomas.flier@no.experis.com>
2024-10-24 15:22:42 +0200

    done

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,14 +1,58 @@
+/* eslint-disable prettier/prettier */
 // do not change these lines
-let adults = 0
-let children = 0
+let adults = 0;
+let children = 0;
+let totalAdults = 0;
+let totalChildren = 0;
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+function enter(numAdults, numChildren) {
+    if (numChildren > numAdults) {
+        return false;
+    }
+    adults += numAdults;
+    children += numChildren;
+    totalAdults += numAdults;
+    totalChildren += numChildren;
+    return true;
+}
+
+function leave(numAdults, numChildren) {
+    if (numChildren > numAdults) {
+        return false;
+    }
+    if (adults - numAdults < children - numChildren) {
+        return false;
+    }
+    if (numAdults > adults || numChildren > children) {
+        return false;
+    }
+    adults -= numAdults;
+    children -= numChildren;
+    return true;
+}
+
+function occupancy() {
+    return {
+        adults: adults,
+        children: children
+    };
+}
+
+function total() {
+    return {
+        adults: totalAdults,
+        children: totalChildren
+    };
+}
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
-}
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
+  total: total
+};
